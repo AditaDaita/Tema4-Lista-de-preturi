589ed0234ad30b2a6c3efd28559b273e90e8644e
AditaDaita <167236736+AditaDaita@users.noreply.github.com>
2024-05-27 02:09:36 +0300

    Lista de preturi.cpp

--- Tema4-Lista de preturi/Tema4-Lista de preturi.cpp
+++ Tema4-Lista de preturi/Tema4-Lista de preturi.cpp
@@ -1,4 +1,4 @@
-// Tema4-Lista de preturi.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// Lista de preturi.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
 #include <iostream>
